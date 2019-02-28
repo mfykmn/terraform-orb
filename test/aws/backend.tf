d6c5008cb3537d63357a317b70d7afb3dcc66a2f
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-02-28 13:08:02 +0900

    Fixed test

---
+++ test/aws/backend.tf
@@ -0,0 +1,14 @@
+#####################################
+# Management of tfstate
+#####################################
+terraform {
+  required_version = "0.11.11"
+
+  backend "s3" {
+    bucket     = "terraform-orb.tfstate"
+    key        = "terraform.tfstate"
+    region     = "us-east-1"
+    lock_table = "terraform-orb.tfstate-lock"
+    encrypt    = true
+  }
+}
