5ad3172f68f012336384f3072f64a2b15d16a7e6
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-02-28 14:11:46 +0900

    Fixed

--- test/backend.tf
+++ test/backend.tf
@@ -5,10 +5,10 @@ terraform {
   required_version = "0.11.11"
 
   backend "s3" {
-    bucket     = "terraform-orb.tfstate"
-    key        = "terraform.tfstate"
-    region     = "us-east-1"
-    lock_table = "terraform-orb.tfstate-lock"
-    encrypt    = true
+    bucket         = "terraform-orb.tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-orb.tfstate-lock"
+    encrypt        = true
   }
 }
